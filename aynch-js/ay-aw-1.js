73a05541631f243eeb86d21b0becc11e6ab855ea
Aman <amanrathi@live.com>
2026-01-12 01:02:41 +0530

    learn async await

---
+++ aynch-js/ay-aw-1.js
@@ -0,0 +1,78 @@
+function fetchUser(userId) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (userId === 5) {
+        resolve({ id: 5, name: "Aman" });
+      } else {
+        reject("User not found");
+      }
+    }, 600);
+  });
+}
+
+function fetchWatchedMovies(userId) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(["Inception", "Interstellar", "Tenet"]);
+    }, 700);
+  });
+}
+
+function fetchRecommendations(watchedMovies) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(["Arrival", "Blade Runner 2049", "The Matrix"]);
+    }, 700);
+  });
+}
+
+// Final result
+// User: Aman
+// Watched: Inception, Interstellar, Tenet
+// Recommendations: Arrival, Blade Runner 2049, The Matrix
+
+// -----------Following results are achived with Async Await-----------
+async function showData() {
+  try {
+    var userDetails = await fetchUser(5);
+    var watchedMovies = await fetchWatchedMovies(userDetails.id);
+    var recommendations = await fetchRecommendations(watchedMovies);
+    console.log(`User: ${userDetails.name}`);
+    console.log(`Watched: ${watchedMovies.join(", ")}`);
+    console.log(`Recommendations: ${recommendations.join(", ")}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+showData();
+
+// -----------Following results are achived with Promises-----------
+// fetchUser(5)
+//   .then(function (user) {
+//     return fetchWatchedMovies(user.id).then(function (watchedMovies) {
+//       return { user: user, watchedMovies: watchedMovies };
+//     });
+//   })
+//   .then(function (data) {
+//     return fetchRecommendations(data.watchedMovies).then(function (
+//       recommendations
+//     ) {
+//       return {
+//         name: data.user.name,
+//         watched: data.watchedMovies,
+//         recommendations: recommendations,
+//       };
+//     });
+//   })
+//   .then(function (finalData) {
+//     console.log(`User: ${finalData.name}`);
+//     console.log(`Watched: ${finalData.watched.join(", ")}`);
+//     console.log(`Recommendations: ${finalData.recommendations.join(", ")}`);
+//   })
+//   .catch(function (err) {
+//     console.log(`Error: ${err}`);
+//   })
+//   .finally(function () {
+//     console.log("And we are done");
+//   });
